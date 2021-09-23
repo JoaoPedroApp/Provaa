6334d3e3c5de84e8e79369f631585cb236e259f6
João <joaopedroapparecido@gmail.com>
2021-09-23 15:02:42 -0300

    ok

--- react/site/src/service/api.js
+++ react/site/src/service/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 const api = axios.create({
-    baseURL: 'https://jpdevstore.herokuapp.com/'
+    baseURL: 'https://jpdevstore.herokuapp.com'
 })
 
 export default class Api {
